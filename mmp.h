0f94cf5ffe834add1c7873b53d90d07c17df2a7a
Robert Lillack <rob@burningsoda.com>
2006-08-18 03:48:00 +0000

    oops. forgot header.
    darcs-hash:20060818034807-2299f-acd1db34aa706f96cafd65a89e84638f4eba4238.gz

---
+++ mmp.h
@@ -0,0 +1,28 @@
+#include <WINGs/WINGs.h>
+
+#define   APP_LONG    "minimalistic music player"
+#define   APP_SHORT   "mmp"
+#define   APP_VERSION   "v0.2"
+
+typedef struct Frontend Frontend;
+typedef struct Backend Backend;
+
+Frontend* feCreate();
+Bool feInit(Frontend*);
+void feAddBackend(Frontend*, Backend*);
+void feRemoveBackend(Frontend*, Backend*);
+void feShowDir(Frontend*, char*);
+void feSetArtist(Frontend*, char*);
+void feSetSongName(Frontend*, char*);
+void fePlayingStopped(Frontend*);
+//   v-- needed?
+void feSetSongLength(Frontend*, unsigned int);
+void feSetCurrentPosition(Frontend*, float, unsigned int, unsigned int);
+
+Backend* beCreate();
+Bool beInit(Backend*);
+void beAddFrontend(Backend*, Frontend*);
+void beRemoveFrontend(Backend*, Frontend*);
+void bePlay(Backend*, char*);
+void beStop(Backend*);
+WMArray* beGetSupportedExtensions(Backend*);
